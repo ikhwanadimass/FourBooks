311b5dd5fcf3ea6c6098de404412634c929e26a0
raziqsiddiq812@gmail.com <your_email@example.com>
2026-05-16 00:25:25 +0700

    Artisan Model dan Atur Kolom Tabel

---
+++ app/Models/Book.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+}
