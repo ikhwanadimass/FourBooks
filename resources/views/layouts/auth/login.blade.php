7ecf78c33e6b3ee95cc6c7588c6ec3f83944d842
Raja Arya <rajaaryapenta@gmail.com>
2026-05-18 22:34:07 +0700

    update login UI

--- resources/views/layouts/auth/login.blade.php
+++ resources/views/layouts/auth/login.blade.php
@@ -9,11 +9,11 @@
             class="fixed inset-0 bg-cover bg-center bg-no-repeat"
             style="background-image: url('{{ asset('images/login-bg.jpg') }}'); background-color: #0a0a0a;"
         >
-            <!-- Dark Overlay - More Transparent to Show Background Image -->
+            <!--Overlay -->
             <div class="absolute inset-0 bg-black/35"></div>
         </div>
 
-        <!-- Content Container -->
+        <!--  Container -->
         <div class="relative z-10 flex min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
             {{ $slot }}
         </div>
